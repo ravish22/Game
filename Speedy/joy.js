a39f0c7abdcff600bc9a03b29012602964190a10
ravish22 <ravish322@gmail.com>
2021-04-16 23:10:35 +0530

    joy stick

---
+++ Speedy/joy.js
@@ -0,0 +1,28 @@
+var joy = new JoyStick('joyDiv',{
+
+    // The ID of canvas element
+    title: 'joystick',
+
+    // width/height
+    width: undefined,
+    height: undefined,
+
+    // Internal color of Stick
+    internalFillColor: '#00AA00',
+
+    // Border width of Stick
+    internalLineWidth: 2,
+
+    // Border color of Stick
+    internalStrokeColor: '#003300',
+
+    // External reference circonference width
+    externalLineWidth: 2,
+
+    //External reference circonference color
+    externalStrokeColor: '#008000',
+
+    // Sets the behavior of the stick
+    autoReturnToCenter: true
+    
+});
