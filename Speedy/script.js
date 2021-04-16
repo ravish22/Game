35da81355f1b7d59094bc8df91592fda9e93cb91
ravish22 <ravish322@gmail.com>
2021-04-16 22:54:40 +0530

    joy stick

--- Speedy/script.js
+++ Speedy/script.js
@@ -3,34 +3,6 @@ const scoreDash = document.querySelector('.scoreDash');
 const lifeDash = document.querySelector('.lifeDash');
 const container = document.getElementById('container');
 const btnStart = document.querySelector('.btnStart');
-var joy = new JoyStick('joyDiv',{
-
-    // The ID of canvas element
-    title: 'joystick',
-
-    // width/height
-    width: undefined,
-    height: undefined,
-
-    // Internal color of Stick
-    internalFillColor: '#00AA00',
-
-    // Border width of Stick
-    internalLineWidth: 2,
-
-    // Border color of Stick
-    internalStrokeColor: '#003300',
-
-    // External reference circonference width
-    externalLineWidth: 2,
-
-    //External reference circonference color
-    externalStrokeColor: '#008000',
-
-    // Sets the behavior of the stick
-    autoReturnToCenter: true
-    
-});
 btnStart.addEventListener('click', startGame);
 document.addEventListener('keydown', pressKeyOn);
 document.addEventListener('keyup', pressKeyOff);
